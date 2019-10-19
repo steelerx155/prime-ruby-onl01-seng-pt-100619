3f46b8cf4e327b362f9b1b4200291f2707f0264f
Randy Steele <steelerx155@gmail.com>
2019-10-19 20:12:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,18 +3,11 @@ def prime?(number)
   if number > 1
     range = (start..number-1).to_a
     range.none? do |num_to_test|
-      
-      
-      
-      
-      
- end
-end 
-
-
-
-
-
-
+ number % num_to_test == 0
+    end
+  else
+    false
+  end
+end
 
 # Add  code here!
